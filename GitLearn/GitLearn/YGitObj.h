e1309b472fbef44534a0a50d7acc4c63d8862113
岳小木 <yupengjie@etouch.cn>
2018-05-11 01:06:36 +0800

    newFile

---
+++ GitLearn/GitLearn/YGitObj.h
@@ -0,0 +1,13 @@
+//
+//  YGitObj.h
+//  GitLearn
+//
+//  Created by 小木 on 2018/5/11.
+//  Copyright © 2018 小木. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YGitObj : NSObject
+
+@end
